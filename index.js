4e6cca60052ac71e1c6e48c81acb5cb8a7a728ab
tygedavis <tyge.davis@gmail.com>
2020-01-14 08:31:49 -0700

    Able to make a GET request to api/users/

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// implement your API here
+// ✔ Implement your API here
+const express = require('express');
+
+const Data = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+// ✔ GET -> Users (general)
+server.get('/api/users', (req, res) => {
+  Data.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    });
+});
+
+//TODO GET -> User (Specific ID)
+server.get('api/users/:id', (req, res) => {
+  const id = req.params.id
+
+  Data.find(id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res.status(404).json({ errorMessage: "The user with that ID does not exist." })
+    })
+})
+
+//TODO POST
+server.post('/api/users', (req, res) => {
+  const userData = req.body;
+
+  Data.add(userData)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+    });
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n API on port ${port}\n`));
